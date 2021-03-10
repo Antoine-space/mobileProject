36cdb8798f8b9ea741619004e2b1db07f4515b90
Antoine ERNEST <antoine.ernest@etudiant.univ-reims.fr>
2021-03-10 08:46:16 +0100

    calendar

--- navigation/index.js
+++ navigation/index.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomBarNav from './BottomBarNavigation';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 function RootNavigation() {
   return (
+    
     <NavigationContainer>
-      <BottomBarNav />
+      <SafeAreaView style={{flex:1 }}>
+        <BottomBarNav />
+      </SafeAreaView>
     </NavigationContainer>
+    
   );
 }
 
--- screens/salaries/Calendar.js
+++ screens/salaries/Calendar.js
@@ -1,14 +1,112 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
 
-const Calendar = () => {
-    return (
-        <View>
-            <Text>Calendar</Text>
-        </View>
-    )
+// const Calendar = () => {
+//     return (
+//         <View>
+//             <Text>Calendar</Text>
+//         </View>
+//     )
+// }
+
+// export default Calendar
+
+// const styles = StyleSheet.create({})
+import React from 'react';
+import {Text, View} from 'react-native';
+import {CalendarList} from 'react-native-calendars';
+
+
+import {LocaleConfig} from 'react-native-calendars';
+
+const colors =  {
+    wait: "orange",
+    accept: "green",
+    decline : "red"
 }
 
-export default Calendar
+LocaleConfig.locales['fr'] = {
+  monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
+  monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
+  dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+  dayNamesShort: ['Dim.','Lun.','Mar.','Mer.','Jeu.','Ven.','Sam.'],
+  today: 'Aujourd\'hui'
+};
+LocaleConfig.defaultLocale = 'fr';
 
-const styles = StyleSheet.create({})
+const CalendarsList = () => {
+
+    const handleAlert = (day) => {
+        return Alert.alert(
+      "Alert Title",
+      "My Alert Msg",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed", day) }
+      ],
+      { cancelable: false }
+    );
+    }
+        
+    
+  return (
+    <CalendarList
+      current={'2021-03-09'}
+      onDayPress={(day) => {handleAlert(day)}}
+      firstDay={1}
+      pastScrollRange={10}
+      futureScrollRange={24}
+      renderHeader={date => {
+        const header = date.toString('MMMM yyyy');
+        const [month, year] = header.split(' ');
+        const textStyle = {
+          fontSize: 18,
+          fontWeight: 'bold',
+          paddingTop: 10,
+          paddingBottom: 10,
+          color: '#5E60CE',
+          paddingRight: 5
+        };
+
+        return (
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              justifyContent: 'space-between',
+              marginTop: 10,
+              marginBottom: 10
+            }}
+          >
+            <Text style={{marginLeft: 5, ...textStyle}}>{`${month}`}</Text>
+            <Text style={{marginRight: 5, ...textStyle}}>{year}</Text>
+          </View>
+        );
+      }}
+      theme={{
+        'stylesheet.calendar.header': {
+          dayHeader: {
+            fontWeight: '600',
+            color: '#48BFE3'
+          }
+        },
+        'stylesheet.day.basic': {
+          today: {
+            borderColor: '#48BFE3',
+            borderWidth: 0.8
+          },
+          todayText: {
+            color: '#5390D9',
+            fontWeight: '800'
+          }
+        }
+      }}
+    />
+  );
+};
+
+export default CalendarsList;
