6b7aab5b028617d7d88eb06cb013f35c4da9144a
Antoine ERNEST <antoine.ernest@etudiant.univ-reims.fr>
2021-03-11 11:02:11 +0100

    env

--- screens/common/Actus.js
+++ screens/common/Actus.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import {AuthContext} from '../../context/AuthContext'
+
 
 const Actus = () => {
+    const {user} = useContext(AuthContext);
     return (
         <View>
             <Text>Actus page</Text>
+            <Text>{user.service}</Text>
         </View>
     )
 }
--- screens/salaries/Account.js
+++ screens/salaries/Account.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useContext, useEffect, useState}from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import {AuthContext} from '../../context/AuthContext'
+
 
 const Account = () => {
+    const { signOut, user } = useContext(AuthContext);
+    const [users, setUsers] = useState([])
+
+    
+  useEffect(() => {
+    const loadUsers = async () => {
+      try {
+        const resp = await fetch(
+          `http://172.20.104.179:3000/api/users`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${user.token}`
+            },
+          }
+        );
+
+        if (!resp.ok) {
+          console.log(resp);
+          return;
+        }
+
+        const respJSON = await resp.json();
+        setUsers(respJSON)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    loadUsers();
+  }, [user]);
+
     return (
         <View>
-            <Text>Account détail me</Text>
+            <Text>Account détail </Text>
+            <Button onPress={signOut} title='logout'></Button>
+
         </View>
     )
 }
