b1f3a67ca703ff9a240a0ce6b2e8db9fe1d2a983
Antoine ERNEST <antoine.ernest@etudiant.univ-reims.fr>
2021-04-02 09:53:54 +0200

    Create img folder

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -9,7 +9,7 @@ import {
   Image
 } from 'react-native';
 import { AuthContext } from '../../context/AuthContext';
-const image = { uri: "../../assets/3348020.png" };
+const image = { uri: "https://i.ibb.co/NYpJHMZ/3348020.png" };
 
 const Login = ({ navigation }) => {
   const { signIn } = useContext(AuthContext);
@@ -28,14 +28,16 @@ const Login = ({ navigation }) => {
     
     <View style={styles.viewAll} >
       <ImageBackground source={image} style={styles.image}>
+        <View style={styles.logoContainer}>
+          <Image source={{ uri: 'https://i.ibb.co/6D59f9P/logo-Fonce.png' }}  style = {styles.logo} />
+        </View>
       <View style={styles.viewLogin} >
-      <Image source={{ uri: 'https://i.ibb.co/6D59f9P/logo-Fonce.png' }}  style = {{ height: 100, width: 100 }} />
-        
           <Text
             style={{
               marginBottom: 50,
               fontSize: 20,
               textAlign: 'center',
+              fontFamily:"SFUIDisplay-Semibold"
             }}
           >
             Login
@@ -74,7 +76,6 @@ export default Login;
 const styles = StyleSheet.create({
   viewAll :{
     flex: 1, 
-    marginHorizontal: 30, 
     justifyContent: "center"
   },
   input: {
@@ -100,12 +101,21 @@ const styles = StyleSheet.create({
     flex: 2, 
     borderRadius: 8,
     padding: 10,
+    marginHorizontal: 30, 
     justifyContent: "center",
-    backgroundColor: 'lightsteelblue',
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2
     }
+  },
+  logo :{
+    width: 150,
+    height: 150,
+  },
+  logoContainer :{
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop:50,
   }
 });
